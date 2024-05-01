9e74f5502c79f494004d6e17917f4ddada98d7ce
Gabriel Pantalhão <pantalhaog@gmail.com>
2024-04-30 21:42:28 -0300

    Atualizando aula06, trabalhando com busca de endereço

--- PPDM/aulas/aula06/app-maps/App.js
+++ PPDM/aulas/aula06/app-maps/App.js
@@ -1,12 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 import MapView, { Marker } from 'react-native-maps';
-import { View } from 'react-native';
+import { TextInput, TouchableOpacity, View } from 'react-native';
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
 
 import {
   requestForegroundPermissionsAsync, // Solicita ao usuário a permissão para utilizar a localização
   getCurrentPositionAsync, // Quando autorizado nos retorna a posição do usuário
   watchPositionAsync, // Observa mudança na localização da posição
   LocationAccuracy,
+  geocodeAsync,
 
 } from 'expo-location';
 import { styles } from "./src/styles/styles";
@@ -14,6 +16,7 @@ import { styles } from "./src/styles/styles";
 
 export default function App() {
   const [location, setLocation] = useState(null);
+  const [searchAdress, setSearchAdress] = useState('');
 
   const mapRef = useRef(null);
 
@@ -21,10 +24,10 @@ export default function App() {
   async function requestLocationPermissions() {
     const { granted } = await requestForegroundPermissionsAsync();
     // Caso o resultado da variável 'granted' seja true, a posição atual é retornada para a variável 'currentPosition'
-
     if (granted) {
       const currentPosition = await getCurrentPositionAsync();
-      setLocation(currentPosition)
+      const { coords: { latitude, longitude } } = currentPosition;
+      setLocation({ latitude, longitude });
       // Exibindo no console as informações da posição atual
       console.log(`LOCALIZAÇÃO ATUAL => `, currentPosition);
     }
@@ -33,6 +36,34 @@ export default function App() {
   useEffect(() => {
     requestLocationPermissions()
   }, []);
+
+  useEffect(() => {
+    mapRef.current?.animateCamera({
+      pitch: 90,
+      center: { latitude: location.latitude, longitude: location.longitude }
+    })
+  }, [location]);
+
+  const searchCoordinates = async (address) => {
+    try {
+      const result = await geocodeAsync(address);
+      console.log('result', result);
+      if (result && result.length > 0) {
+        const { latitude, longitude } = result[0];
+        // Atualização a localização no estado
+        setLocation({ latitude, longitude });
+        // console.log('Resuldado da pesquisa:' , latitude, longitude);
+        mapRef.current?.animateCamera({
+          pitch: 50,
+          center: { latitude, longitude }
+        })
+      } else {
+        console.log('Endereço não encontrado')
+      }
+    } catch (error) {
+      console.error('Erro ao converter endereço', error);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -43,21 +74,33 @@ export default function App() {
           ref={mapRef}
           style={styles.map}
           initialRegion={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
+            latitude: location.latitude,
+            longitude: location.longitude,
             latitudeDelta: 0.05,
-            longitudeDelta: 0.05
+            longitudeDelta: 0.05,
           }}
         >
           <Marker
             coordinate={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude
+              latitude: location.latitude,
+              longitude: location.longitude,
             }}
           />
         </MapView>
       }
+      <View style={styles.searchContainer}>
+        <View style={styles.searchInput}>
+          <TextInput
+            style={{ flex: 1 }}
+            placeholder="Digite o endereço"
+            value={searchAdress}
+            onChangeText={(text) => setSearchAdress(text)}
+          />
+          <TouchableOpacity style={styles.serchButton} onPress={() => searchCoordinates(searchAdress)}>
+            <FontAwesome6 name='magnifying-glass' size={26} color='#BDC9DE'></FontAwesome6>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
-
--- PPDM/aulas/aula06/app-maps/src/styles/styles.js
+++ PPDM/aulas/aula06/app-maps/src/styles/styles.js
@@ -1,6 +1,5 @@
-import { StyleSheet } from 'react-native';
-
-
+import { Platform, StyleSheet } from 'react-native';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 export const styles = StyleSheet.create({
     container: {
@@ -9,8 +8,38 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+
     map: {
         flex: 1,
         width: '100%'
-    }
+    },
+
+    searchContainer: {
+        position: 'absolute',
+        top: Platform.OS === 'android' ? getStatusBarHeight() : 0,
+        left: 10,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+    },
+
+    searchInput: {
+        width: '83%',
+        flexDirection: 'row',
+        height: 40,
+        borderColor: '#fff',
+        borderRadius: 10,
+        backgroundColor: 'white',
+        elevation: 10,
+        padding: 5,
+        paddingRight: 40,
+        marginTop: 40,
+        marginLeft: 40,
+    },
+
+    serchButton: {
+        padding: 7,
+        position: 'absolute',
+        right: 0,
+        transform: [{ rotate: '90deg'}],
+    },
 });
